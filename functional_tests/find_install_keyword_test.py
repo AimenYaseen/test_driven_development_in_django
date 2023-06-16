44a8b8a0f9e7cc7852d8e642fec9589277ae0972
AimenYaseen <aimenyaseen6@gmail.com>
2023-06-16 11:06:27 +0500

    added time sleep so that the chrome stable for sometime

--- functional_tests/find_install_keyword_test.py
+++ functional_tests/find_install_keyword_test.py
@@ -1,6 +1,11 @@
+import time
 from selenium import webdriver
 
 browser = webdriver.Chrome()
 browser.get('http://localhost:8000')
 
+time.sleep(5)  # Let the user actually see something!
+
 assert browser.page_source.find("install")
+
+time.sleep(5)  # Let the user actually see something!
