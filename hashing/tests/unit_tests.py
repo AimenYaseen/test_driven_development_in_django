7f872652814b998fcc015e9529047e26188d315e
AimenYaseen <aimenyaseen6@gmail.com>
2023-06-17 10:54:31 +0500

    added UnitTestCase class and test home_homepage_template

---
+++ hashing/tests/unit_tests.py
@@ -0,0 +1,10 @@
+from django.test import TestCase
+
+
+class UnitTestCase(TestCase):
+
+    def test_home_homepage_template(self):
+        """
+        checks whether the home template exists
+        """
+
