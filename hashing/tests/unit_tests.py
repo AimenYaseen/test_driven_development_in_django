0027b09c26a398c2bd9e6fcc0492b65d4e587885
AimenYaseen <aimenyaseen6@gmail.com>
2023-06-18 21:45:27 +0500

    added a new test 'test_hash_form'. It will fail at this point as there is no HashForm

--- hashing/tests/unit_tests.py
+++ hashing/tests/unit_tests.py
@@ -9,3 +9,12 @@ class UnitTestCase(TestCase):
         """
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'hashing/home.html')
+
+    def test_hash_form(self):
+        """
+        checks two things
+        1. HashForm exists
+        2. accepts a text value and form is valid
+        """
+        form = HashForm(data={'text': 'hello'})
+        self.assertTrue(form.is_valid)
