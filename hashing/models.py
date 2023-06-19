c0228b077d1b6f853dbf34fc7120884715f28864
AimenYaseen <aimenyaseen6@gmail.com>
2023-06-19 10:02:32 +0500

    added Hash model

--- hashing/models.py
+++ hashing/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Hash(models.Model):
+    """
+    This model stores the text and its hash in lower case
+    """
+    text = models.TextField()
+    hash = models.CharField(max_length=64)
