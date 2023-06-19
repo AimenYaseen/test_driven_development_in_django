9bfa78f809d8e0434bdb63586380848caf89d18f
AimenYaseen <aimenyaseen6@gmail.com>
2023-06-19 10:50:34 +0500

    added hash url

--- hashthat/urls.py
+++ hashthat/urls.py
@@ -5,5 +5,6 @@ from hashing import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('hash/<str:hash_text>', views.hashing, name='hash'),
 ]
